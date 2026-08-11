22b888ac780bfa1acf5c0ff0000ce57edd97e23a
Bhautik Sorathiya <bhautiksorthiya8@gmail.com>
2026-08-10 23:38:57 -0400

    test: add k6 concurrency load test proving no overselling under 100 simultaneous reservation requests

--- loadtests/SELECT Id, QuantityOnHand, QuantityReser.sql
+++ loadtests/SELECT Id, QuantityOnHand, QuantityReser.sql
@@ -1,3 +1,3 @@
-SELECT Id, QuantityOnHand, QuantityReserved, QuantityAvailable
+SELECT Id, QuantityOnHand, QuantityReserved, (QuantityOnHand - QuantityReserved) AS QuantityAvailable
 FROM InventoryBatches
 WHERE BatchNumber = 'LOADTEST-001';
